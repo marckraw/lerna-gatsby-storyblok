af94f050e751dedb22fc2f00c1bdafcb37550856
Marcin Krawczyk <marckraw@icloud.com>
2020-07-26 19:24:14 +0200

    Add package level netlify.toml

--- plugins/netlify-plugin-hello-world/index.js
+++ plugins/netlify-plugin-hello-world/index.js
@@ -1,8 +1,10 @@
 // index.js
 
 module.exports = {
-    onPreBuild: () => {
+    onPreBuild: (things) => {
       console.log('Hello world from onPreBuild event!')
       console.log("twoja starar pierze w rzece")
+      console.log("wtf ? ")
+      console.log(things);
     },
   }
