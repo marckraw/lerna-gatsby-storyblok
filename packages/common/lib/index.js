a6fd2e616e75ee492aef150ed5aaa81415bda03d
Marcin Krawczyk <marckraw@icloud.com>
2020-07-26 20:52:54 +0200

    remove problematic scss file not being copied to lib right now

--- packages/common/lib/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Heading } from "./Heading";
-export { default as CommonTextBlock } from "./components/text-block/text-block";
--- packages/common/src/components/text-block/text-block.js
+++ packages/common/src/components/text-block/text-block.js
@@ -1,7 +1,6 @@
 import React from "react";
 import SbEditable from "storyblok-react";
 import ReactMardown from "react-markdown";
-import './text-block.scss'
 
 const TextBlock = (props) => {
   console.log("give me props: ");
@@ -27,24 +26,5 @@ const TextBlock = (props) => {
     </SbEditable>
   );
 };
-//   return (
-//     <SbEditable content={props.blok}>
-//       <div className="text-block">
-//         {props.blok.title && (
-//           <TitleTag className={`ef-h${props.blok.visual_title_size}`}>
-//             {props.blok.title}
-//           </TitleTag>
-//         )}
-//         <div
-//           className={`text-block--${
-//             props.blok.text_weight === 'light' ? 'light' : ''
-//           }`}
-//         >
-//           <ReactMardown source={props.blok.content} className="markdown" />
-//         </div>
-//       </div>
-//     </SbEditable>
-//   );
-// };
 
 export default TextBlock;
